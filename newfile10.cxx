dd21694750ab96a72cc519e38ce8039f89508048
oscarGR-123 <j.oscar.rf.223@gmail.com>
2025-03-20 08:52:19 -0600

    Add files via upload

---
+++ newfile10.cxx
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	double D = 10;
+	double r = D/2;
+	double pi = 3.1416;
+	double A = pi*r*r;
+	printf("%f" , A );
+	
+	printf("\n");
+	system("Pause");
+	return 0;
+}
